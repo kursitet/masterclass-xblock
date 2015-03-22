7571a441e5f628363c53f337ed5353cb56451c57
Eugene Medvedev <rn3aoh.g@gmail.com>
2015-03-22 18:19:43 +0300

    Logic, dammit.

--- masterclass/masterclass.py
+++ masterclass/masterclass.py
@@ -149,7 +149,7 @@ class MasterclassXBlock(XBlock):
             return u"Ваша заявка ожидает одобрения преподавателем."
         if self.has_ended():
             return u"Прием заявок окончен."
-        if not self.free_capacity():
+        if not self.free_capacity() and not self.approval_required:
             return u"Извините, свободных мест больше нет. Если кто-то из других слушателей отзовет свою заявку, " \
                    u"или количество свободных мест будет увеличено, они могут появиться в будущем."
         return u"Вы можете зарегистрироваться на этот мастер-класс."
